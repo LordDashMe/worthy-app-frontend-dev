91c73e4e14c3d108c53a133b35913b57772b34a6
LordDashMe <reyesjoshuaclifford@gmail.com>
2020-09-14 18:52:29 +0800

    build: add UX optimization

--- resources/src/scripts/main.js
+++ resources/src/scripts/main.js
@@ -7,20 +7,25 @@
         data_source_entry: '/data-source/entry'
     };
 
-    var setStatusText = function (text) {
+    var setLogs = function (text) {
 
-        var status = document.getElementById('status');
-        
-        if (text === 'IDLE') {
-            status.classList.remove('-processing');
-            status.classList.add('-idle');
-        } else {
-            status.classList.remove('-idle');
-            status.classList.add('-processing');
-        }
+        var date = new Date();
 
-        status.textContent = text;
-        
+        var format =
+            date.getFullYear() + '-' +
+            ('00' + (date.getMonth() + 1)).slice(-2) + '-' +
+            ('00' + date.getDate()).slice(-2) + ' ' +
+            ('00' + date.getHours()).slice(-2) + ':' +
+            ('00' + date.getMinutes()).slice(-2) + ':' +
+            ('00' + date.getSeconds()).slice(-2);
+
+        document.getElementById('logs').value += '[' + format + ']: ' + text + '\n';
+    };
+
+    var setStatus = function (status) {
+
+        document.getElementById('status').textContent = '[' + status.toUpperCase() + ']';
+
     };
 
     var defaultValueBasedOnDataType = function (dataType) {
@@ -47,44 +52,54 @@
 
     };
 
+    var fetchResponseHandler = function (response) {
+        
+        if (!response.ok) {
+            return Promise.reject(response);
+        }
+
+        return response.json();
+
+    };
+
+    var fetchErrorHandler = function (error) {
+
+        error.json().then(function (result) {
+
+            alert(result.message);
+            console.error(result.message);
+            setLogs('DONE');
+        
+        });
+
+    };
+
     var handleAuthAction = function () {
 
         var userName = document.getElementById('username').value;
         var password = document.getElementById('password').value;
 
-        setStatusText('LOADING AUTH...');
+        setLogs('LOADING AUTH');
+
+        var options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json', 
+                'X-WORTHY-APP-HEADER': 1
+            },
+            body: JSON.stringify({
+                'userName': userName, 
+                'password': password
+            })
+        };
         
-        fetch(API_URL_CONFIG.source + API_URL_CONFIG.auth,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json', 
-                    'X-WORTHY-APP-HEADER': 1
-                },
-                body: JSON.stringify({
-                    'userName': userName, 
-                    'password': password
-                })
-            }
-        )
-        .then(function (response) {
-            
-            if (!response.ok) {
-                throw new Error('Please check network response.');
-            }
-
-            return response.json();
-
-        })
-        .then(function (result) {
-            document.getElementById('token').value = result.token;
-
-            setStatusText('IDLE');
-        })
-        .catch(function (error) {
-          console.error('Error:', error);
-          setStatusText('IDLE');
-        });
+        fetch(API_URL_CONFIG.source + API_URL_CONFIG.auth, options)
+            .then(fetchResponseHandler)
+            .then(function (result) {
+                document.getElementById('token').value = result.token;
+                setLogs('DONE');
+            })
+            .catch(fetchErrorHandler);
         
     };
 
@@ -93,53 +108,38 @@
         var token = document.getElementById('token').value;
         var systemName = document.getElementById('data_source_system_name').value;
 
-        setStatusText('LOADING DATA SOURCE...');
+        setLogs('LOADING DATA SOURCE: ' + systemName);
 
-        fetch(API_URL_CONFIG.source + API_URL_CONFIG.data_source + '/' + systemName,
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json', 
-                    'X-WORTHY-APP-HEADER': 1,
-                    'X-WORTHY-APP-JWT': token
+        var options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json', 
+                'X-WORTHY-APP-HEADER': 1,
+                'X-WORTHY-APP-JWT': token
+            }
+        };
+
+        fetch(API_URL_CONFIG.source + API_URL_CONFIG.data_source + '/' + systemName, options)
+            .then(fetchResponseHandler)
+            .then(function (result) {
+            
+                document.getElementById('current_data_source').value = systemName;
+                document.getElementById('current_action').value = action;
+
+                var fields = {};
+
+                for (var x in result.fields) {
+                    fields[result.fields[x]['systemName']] = defaultValueBasedOnDataType(result.fields[x]['dataType']);
                 }
-            }
-        )
-        .then(function (response) {
 
-            if (!response.ok) {
-                throw new Error('Please check network response.');
-            }
+                document.getElementById('details').value = JSON.stringify({
+                    "systemName": systemName,
+                    "fields": fields
+                }, null, 4); 
 
-            return response.json();
-        })
-        .then(function (result) {
-
-            if (!result || typeof result.fields === 'undefined') {
-                throw new Error('Please check network response.');
-            }
-           
-            document.getElementById('current_data_source').value = systemName;
-            document.getElementById('current_action').value = action;
-
-            var fields = {};
-
-            for (var x in result.fields) {
-                fields[result.fields[x]['systemName']] = defaultValueBasedOnDataType(result.fields[x]['dataType']);
-            }
-
-            document.getElementById('details').value = JSON.stringify({
-                "systemName": systemName,
-                "fields": fields
-            }, null, 4); 
-
-            setStatusText('IDLE');
-
-        })
-        .catch(function (error) {
-          console.error('Error:', error);
-          setStatusText('IDLE');
-        });
+                setLogs('DONE');
+            })
+            .catch(fetchErrorHandler);
 
     };
 
@@ -149,38 +149,33 @@
         var action = document.getElementById('current_action').value;
         var details = document.getElementById('details').value;
 
-        setStatusText('LOADING COMMIT...');
+        setLogs('EXECUTING COMMIT');
+        setLogs(details);
+        setStatus('PROCESSING');
 
-        fetch(API_URL_CONFIG.source + API_URL_CONFIG.data_source_entry,
-            {
-                method: requestMethodBasedOnAction(action),
-                headers: {
-                    'Content-Type': 'application/json', 
-                    'X-WORTHY-APP-HEADER': 1,
-                    'X-WORTHY-APP-JWT': token
-                },
-                body: details 
-            }
-        )
-        .then(function (response) {
+        var options = {
+            method: requestMethodBasedOnAction(action),
+            headers: {
+                'Content-Type': 'application/json', 
+                'X-WORTHY-APP-HEADER': 1,
+                'X-WORTHY-APP-JWT': token
+            },
+            body: details 
+        };
 
-            if (!response.ok) {
-                throw new Error('Please check network response.');
-            }
-
-            return response.json();
-        })
-        .then(function (result) {
-            alert(result.message);
-            setStatusText('IDLE');
-        })
-        .catch(function (error) {
-          console.error('Error:', error);
-          setStatusText('IDLE');
-        });
+        fetch(API_URL_CONFIG.source + API_URL_CONFIG.data_source_entry, options)
+            .then(fetchResponseHandler)
+            .then(function (result) {
+                alert(result.message);
+                setLogs('DONE');
+                setStatus('IDLE');
+                return;
+            })
+            .catch(fetchErrorHandler);
 
     };
 
+    // EVENT BINDINGS
     document.getElementById('get_token').addEventListener('click', handleAuthAction);
     document.getElementById('set_create_action').addEventListener('click', handleDataSource.bind(this, 'CREATE'));
     document.getElementById('commit_action').addEventListener('click', handleCommit);
