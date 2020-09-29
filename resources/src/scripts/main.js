c99dd0f956b7ae2de93f21184362eda55cd31125
LordDashMe <reyesjoshuaclifford@gmail.com>
2020-09-29 22:10:45 +0800

    build: add optimized version

--- resources/src/scripts/main.js
+++ resources/src/scripts/main.js
@@ -7,6 +7,10 @@
         data_source_entry: '/data-source/entry'
     };
 
+    var bumpAlert = function () {
+        alert('Done, please check logs for more info.');
+    };
+
     var setLogs = function (text) {
 
         var date = new Date();
@@ -19,7 +23,10 @@
             ('00' + date.getMinutes()).slice(-2) + ':' +
             ('00' + date.getSeconds()).slice(-2);
 
-        document.getElementById('logs').value += '[' + format + ']: ' + text + '\n';
+        var textarea = document.getElementById('logs');
+
+        textarea.value += '[' + format + ']: ' + text + '\n';
+        textarea.scrollTop = textarea.scrollHeight;
     };
 
     var setStatus = function (status) {
@@ -41,17 +48,6 @@
 
     };
 
-    var requestMethodBasedOnAction = function (actionType) {
-
-        switch (actionType) {
-            case 'CREATE':
-                return 'POST';
-            case 'UPDATE':
-                return 'PATCH';
-        }
-
-    };
-
     var fetchResponseHandler = function (response) {
         
         if (!response.ok) {
@@ -64,13 +60,19 @@
 
     var fetchErrorHandler = function (error) {
 
-        error.json().then(function (result) {
+        if (typeof error.json === 'function') {
 
-            alert(result.message);
-            console.error(result.message);
-            setLogs('ERROR: ' + result.message);
-        
-        });
+            error.json().then(function (result) {
+
+                alert(result.message);
+                console.error(result.message);
+                setLogs('ERROR: ' + result.message);
+            
+            });
+
+        } else {
+            console.error(error);
+        }
 
     };
 
@@ -96,6 +98,7 @@
         fetch(API_URL_CONFIG.source + API_URL_CONFIG.auth, options)
             .then(fetchResponseHandler)
             .then(function (result) {
+                bumpAlert();
                 document.getElementById('token').value = result.token;
                 setLogs('DONE');
             })
@@ -119,12 +122,16 @@
             }
         };
 
-        fetch(API_URL_CONFIG.source + API_URL_CONFIG.data_source + '/' + systemName, options)
+        var url = API_URL_CONFIG.source + API_URL_CONFIG.data_source + '/' + systemName;
+
+        fetch(url, options)
             .then(fetchResponseHandler)
             .then(function (result) {
+
+                bumpAlert();
             
-                document.getElementById('current_data_source').value = systemName;
-                document.getElementById('current_action').value = action;
+                document.getElementById('data_source_url').value = url;
+                document.getElementById('data_source_action').value = action;
 
                 var fields = {};
 
@@ -132,9 +139,9 @@
                     fields[result.fields[x]['systemName']] = defaultValueBasedOnDataType(result.fields[x]['dataType']);
                 }
 
-                document.getElementById('details').value = JSON.stringify({
-                    "systemName": systemName,
-                    "fields": fields
+                document.getElementById('data_source_body').value = JSON.stringify({
+                    'systemName': systemName,
+                    'fields': fields
                 }, null, 4); 
 
                 setLogs('DONE');
@@ -146,27 +153,31 @@
     var handleCommit = function () {
 
         var token = document.getElementById('token').value;
-        var action = document.getElementById('current_action').value;
-        var details = document.getElementById('details').value;
+        var dataSourceAction = document.getElementById('data_source_action').value;
+        var dataSourceBody = document.getElementById('data_source_body').value;
 
         setLogs('EXECUTING COMMIT');
-        setLogs(details);
+        setLogs(dataSourceBody);
         setStatus('PROCESSING');
 
         var options = {
-            method: requestMethodBasedOnAction(action),
+            method: dataSourceAction,
             headers: {
                 'Content-Type': 'application/json', 
                 'X-WORTHY-APP-HEADER': 1,
                 'X-WORTHY-APP-JWT': token
-            },
-            body: details 
+            } 
         };
+
+        if (dataSourceAction === 'POST' || dataSourceAction === 'PATCH') {
+            options['body'] = dataSourceBody;
+        }
 
         fetch(API_URL_CONFIG.source + API_URL_CONFIG.data_source_entry, options)
             .then(fetchResponseHandler)
             .then(function (result) {
-                alert(result.message);
+                bumpAlert();
+                document.getElementById('data_source_output').value = JSON.stringify(result, null, 4);
                 setLogs('DONE');
                 setStatus('IDLE');
                 return;
@@ -177,7 +188,7 @@
 
     // EVENT BINDINGS
     document.getElementById('get_token').addEventListener('click', handleAuthAction);
-    document.getElementById('set_create_action').addEventListener('click', handleDataSource.bind(this, 'CREATE'));
+    document.getElementById('set_post_action').addEventListener('click', handleDataSource.bind(this, 'POST'));
     document.getElementById('commit_action').addEventListener('click', handleCommit);
 
 })();
