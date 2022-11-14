bac3b007b747a395077d55a69d5dd611b1503db5
LordDashMe <reyesjoshuaclifford@gmail.com>
2022-11-14 15:37:05 +0800

    Add adjustments for worthy app version 1.0.0

--- resources/src/scripts/main.js
+++ resources/src/scripts/main.js
@@ -1,236 +1,266 @@
 (function () {
 
-    var API_URL_CONFIG = {
-        source: 'https://worthy-app.onrender.com',
-        auth: '/user/login',
-        data_source: '/data-source'
-    };
+  var API_URL_CONFIG = {
+    source: 'https://worthy-app.onrender.com',
+    auth: '/user/login',
+    data_source: '/data-source'
+  };
 
-    var getDataSourceURL = function (systemName) {
-        return API_URL_CONFIG.data_source + '/' + systemName;   
-    };
+  var isProcessing = false;
 
-    var getDataSourceEntriesURL = function (systemName) {
-        return API_URL_CONFIG.data_source + '/' + systemName + '/entries';
-    };
+  var getDataSourceURL = function (systemName) {
+    return API_URL_CONFIG.data_source + '/' + systemName;   
+  };
 
-    var getDataSourceEntryURL = function () {
-        return API_URL_CONFIG.data_source + '/entry';
+  var getDataSourceEntryURL = function (systemName) {
+    return API_URL_CONFIG.data_source + '/' + systemName + '/entry';
+  };
+
+  var bumpAlert = function () {
+    alert('Done, please check logs for more info.');
+  };
+
+  var setLogs = function (text) {
+
+    var date = new Date();
+
+    var format = (
+      date.getFullYear() + '-' +
+      ('00' + (date.getMonth() + 1)).slice(-2) + '-' +
+      ('00' + date.getDate()).slice(-2) + ' ' +
+      ('00' + date.getHours()).slice(-2) + ':' +
+      ('00' + date.getMinutes()).slice(-2) + ':' +
+      ('00' + date.getSeconds()).slice(-2)
+    );
+
+    var textarea = document.getElementById('logs');
+
+    textarea.value += '[' + format + ']: ' + text + '\n';
+    textarea.scrollTop = textarea.scrollHeight;
+
+  };
+
+  var setStatus = function (status) {
+    document.getElementById('status').textContent = '[' + status.toUpperCase() + ']';
+  };
+
+  var defaultValueBasedOnDataType = function (dataType) {
+
+    switch (dataType) {
+      case 'String':
+        return '';
+      case 'Date':
+        return '0000-00-00';
+      case 'Number':
+        return 0;
     }
 
-    var bumpAlert = function () {
-        alert('Done, please check logs for more info.');
+  };
+
+  var fetchResponseHandler = function (response) {
+      
+    if (!response.ok) {
+      return Promise.reject(response);
+    }
+
+    return response.json();
+
+  };
+
+  var fetchErrorHandler = function (error) {
+
+    var defaultErrorHandling = function (error) {
+      alert('Something\'s went wrong, please try again later.');
+      console.error(error);
+      setLogs('(SYS MSG) RESPONSE ERROR: Something\'s went wrong, please try again later.');
+      setStatus('IDLE');
+      isProcessing = false;
     };
 
-    var setLogs = function (text) {
+    if (error.status === 404) {
+      defaultErrorHandling(error);
+      return;
+    }
 
-        var date = new Date();
+    if (typeof error.json === 'function') {
 
-        var format =
-            date.getFullYear() + '-' +
-            ('00' + (date.getMonth() + 1)).slice(-2) + '-' +
-            ('00' + date.getDate()).slice(-2) + ' ' +
-            ('00' + date.getHours()).slice(-2) + ':' +
-            ('00' + date.getMinutes()).slice(-2) + ':' +
-            ('00' + date.getSeconds()).slice(-2);
+      error.json().then(function (result) {
 
-        var textarea = document.getElementById('logs');
+        document.getElementById('data_source_output').value = result.message;
 
-        textarea.value += '[' + format + ']: ' + text + '\n';
-        textarea.scrollTop = textarea.scrollHeight;
+        alert(result.message);
+        console.error(result.message);
+        setLogs('(SYS MSG) RESPONSE ERROR: ' + result.message);
+        setStatus('IDLE');
+        isProcessing = false;
+      
+      });
+
+    } else {
+      defaultErrorHandling(error);
+    }
+
+  };
+
+  var handleAuthAction = function () {
+
+    if (isProcessing) {
+      return;
+    }
+
+    isProcessing = true;
+
+    var userName = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    setLogs('(SYS MSG) LOADING AUTH');
+
+    var options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', 
+        'X-WORTHY-APP-HEADER': 1
+      },
+      body: JSON.stringify({
+        'userName': userName, 
+        'password': password
+      })
     };
+    
+    fetch(API_URL_CONFIG.source + API_URL_CONFIG.auth, options)
+      .then(fetchResponseHandler)
+      .then(function (result) {
+        bumpAlert();
+        document.getElementById('token').value = result.token;
+        setLogs('(SYS MSG) SUCCESS RESPONSE: Done, please check the Token field.');
+        isProcessing = false;
+      })
+      .catch(fetchErrorHandler);
 
-    var setStatus = function (status) {
+  };
 
-        document.getElementById('status').textContent = '[' + status.toUpperCase() + ']';
+  var handleDataSource = function (action) {
 
-    };
+    if (isProcessing) {
+      return;
+    }
 
-    var defaultValueBasedOnDataType = function (dataType) {
+    isProcessing = true;
 
-        switch (dataType) {
-            case 'String':
-                return '';
-            case 'Date':
-                return '0000-00-00';
-            case 'Number':
-                return 0;
-        }
+    var token = document.getElementById('token').value;
+    var systemName = document.getElementById('data_source_system_name').value;
 
-    };
+    setLogs('(SYS MSG) LOADING DATA SOURCE: ' + systemName + ' with action ' + action);
 
-    var fetchResponseHandler = function (response) {
-        
-        if (!response.ok) {
-            return Promise.reject(response);
-        }
+    var postActionResponse = function () {
 
-        return response.json();
+      document.getElementById('data_source_form_body').hidden = false;
+      document.getElementById('data_source_url').value = API_URL_CONFIG.source + getDataSourceEntryURL(systemName);
+      document.getElementById('data_source_action').value = action;
 
-    };
+      var url = API_URL_CONFIG.source + getDataSourceURL(systemName);
 
-    var fetchErrorHandler = function (error) {
+      var options = {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json', 
+              'X-WORTHY-APP-HEADER': 1,
+              'X-WORTHY-APP-JWT': token
+          }
+      };
 
-        if (typeof error.json === 'function') {
+      fetch(url, options)
+        .then(fetchResponseHandler)
+        .then(function (result) {
 
-            error.json().then(function (result) {
+          bumpAlert();
 
-                document.getElementById('data_source_output').value = result.message;
+          var fields = {};
+          for (var x in result.fields) {
+              fields[result.fields[x]['systemName']] = defaultValueBasedOnDataType(result.fields[x]['dataType']);
+          }
 
-                alert(result.message);
-                console.error(result.message);
-                setLogs('ERROR: ' + result.message);
-            
-            });
+          document.getElementById('data_source_body').value = JSON.stringify(fields, null, 2);
 
-        } else {
-            console.error(error);
-        }
+          setLogs('(SYS MSG) SUCCESS RESPONSE: Done, Please check the Data Source Body field.');
+          isProcessing = false;
 
-    };
-
-    var handleAuthAction = function () {
-
-        var userName = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-
-        setLogs('LOADING AUTH');
-
-        var options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json', 
-                'X-WORTHY-APP-HEADER': 1
-            },
-            body: JSON.stringify({
-                'userName': userName, 
-                'password': password
-            })
-        };
-        
-        fetch(API_URL_CONFIG.source + API_URL_CONFIG.auth, options)
-            .then(fetchResponseHandler)
-            .then(function (result) {
-                bumpAlert();
-                document.getElementById('token').value = result.token;
-                setLogs('DONE');
-            })
-            .catch(fetchErrorHandler);
-        
-    };
-
-    var handleDataSource = function (action, event) {
-
-        var token = document.getElementById('token').value;
-        var systemName = document.getElementById('data_source_system_name').value;
-
-        setLogs('LOADING DATA SOURCE: ' + systemName + ' with action ' + action);
-
-        var postActionResponse = function () {
-
-            document.getElementById('data_source_form_body').hidden = false;
-
-            document.getElementById('data_source_url').value = API_URL_CONFIG.source + getDataSourceEntryURL();
-            document.getElementById('data_source_action').value = action;
-
-            var url = API_URL_CONFIG.source + getDataSourceURL(systemName);
-
-            var options = {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json', 
-                    'X-WORTHY-APP-HEADER': 1,
-                    'X-WORTHY-APP-JWT': token
-                }
-            };
-
-            fetch(url, options)
-                .then(fetchResponseHandler)
-                .then(function (result) {
-
-                    bumpAlert();
-
-                    var fields = {};
-
-                    for (var x in result.fields) {
-                        fields[result.fields[x]['systemName']] = defaultValueBasedOnDataType(result.fields[x]['dataType']);
-                    }
-
-                    document.getElementById('data_source_body').value = JSON.stringify({
-                        'systemName': systemName,
-                        'fields': fields
-                    }, null, 4);
-
-                    setLogs('DONE');
-                })
-                .catch(fetchErrorHandler);
-
-        };
-
-        var getActionResponse = function () {
-
-            document.getElementById('data_source_form_body').hidden = true;
-
-            document.getElementById('data_source_url').value = API_URL_CONFIG.source + getDataSourceEntriesURL(systemName);
-            document.getElementById('data_source_action').value = action;
-
-        };
-
-        if (action === 'POST') {
-            postActionResponse();
-        } else if (action === 'GET') {
-            getActionResponse();
-        }
+        })
+        .catch(fetchErrorHandler);
 
     };
 
-    var handleCommit = function () {
+    var getActionResponse = function () {
 
-        var token = document.getElementById('token').value;
+      document.getElementById('data_source_form_body').hidden = true;
+      document.getElementById('data_source_url').value = API_URL_CONFIG.source + getDataSourceEntryURL(systemName);
+      document.getElementById('data_source_action').value = action;
 
-        var dataSourceUrl = document.getElementById('data_source_url').value;
-        var dataSourceAction = document.getElementById('data_source_action').value;
-
-        var dataSourceBody = document.getElementById('data_source_body').value;
-
-        setLogs('EXECUTING COMMIT');
-
-        if (dataSourceAction === 'POST') {
-            setLogs(dataSourceBody);
-        }
-        
-        setStatus('PROCESSING');
-
-        var options = {
-            method: dataSourceAction,
-            headers: {
-                'Content-Type': 'application/json', 
-                'X-WORTHY-APP-HEADER': 1,
-                'X-WORTHY-APP-JWT': token
-            } 
-        };
-
-        if (dataSourceAction === 'POST' || dataSourceAction === 'PATCH') {
-            options['body'] = dataSourceBody;
-        }
-
-        fetch(dataSourceUrl, options)
-            .then(fetchResponseHandler)
-            .then(function (result) {
-                bumpAlert();
-                document.getElementById('data_source_output').value = JSON.stringify(result, null, 4);
-                setLogs('DONE');
-                setStatus('IDLE');
-                return;
-            })
-            .catch(fetchErrorHandler);
+      isProcessing = false;
 
     };
 
-    // EVENT BINDINGS
-    document.getElementById('get_token').addEventListener('click', handleAuthAction);
-    document.getElementById('set_get_action').addEventListener('click', handleDataSource.bind(this, 'GET'));
-    document.getElementById('set_post_action').addEventListener('click', handleDataSource.bind(this, 'POST'));
-    document.getElementById('commit_action').addEventListener('click', handleCommit);
+    if (action === 'POST') {
+      postActionResponse();
+    } else if (action === 'GET') {
+      getActionResponse();
+    }
+
+  };
+
+  var handleCommit = function () {
+
+    if (isProcessing) {
+      return;
+    }
+
+    setLogs('(SYS MSG) COMMIT ACTION TRIGGERED');
+    setStatus('PROCESSING');
+
+    isProcessing = true;
+
+    document.getElementById('data_source_output').value = '';
+
+    var token = document.getElementById('token').value;
+    var dataSourceUrl = document.getElementById('data_source_url').value;
+    var dataSourceAction = document.getElementById('data_source_action').value;
+    var dataSourceBody = document.getElementById('data_source_body').value;
+
+    setLogs('Action: ' + dataSourceAction);
+    if (dataSourceAction === 'POST') {
+      setLogs('Request Body: \n' + dataSourceBody);
+    }
+
+    var options = {
+      method: dataSourceAction,
+      headers: {
+        'Content-Type': 'application/json', 
+        'X-WORTHY-APP-HEADER': 1,
+        'X-WORTHY-APP-JWT': token
+      } 
+    };
+
+    if (dataSourceAction === 'POST' || dataSourceAction === 'PATCH') {
+      options['body'] = dataSourceBody;
+    }
+
+    fetch(dataSourceUrl, options)
+      .then(fetchResponseHandler)
+      .then(function (result) {
+        bumpAlert();
+        document.getElementById('data_source_output').value = JSON.stringify(result, null, 4);
+        setLogs('(SYS MSG) SUCCESS RESPONSE: Done, Please check the Output field.');
+        setStatus('IDLE');
+        isProcessing = false;
+      })
+      .catch(fetchErrorHandler);
+
+  };
+
+  // EVENT BINDINGS
+  document.getElementById('get_token').addEventListener('click', handleAuthAction);
+  document.getElementById('set_get_action').addEventListener('click', handleDataSource.bind(this, 'GET'));
+  document.getElementById('set_post_action').addEventListener('click', handleDataSource.bind(this, 'POST'));
+  document.getElementById('commit_action').addEventListener('click', handleCommit);
 
 })();
